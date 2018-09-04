f98a162afbb40e9056b27a6d050010ea985c86cb
Ceri Hughes <github@cerihughes.co.uk>
2018-09-04 14:47:24 +0100

    Implement touch handling to cycle through terrains

--- Sentinel/ViewController.swift
+++ Sentinel/ViewController.swift
@@ -2,6 +2,9 @@ import SceneKit
 
 class ViewController: UIViewController {
 
+    var terrainIndex = 0
+    var terrainNode = SCNNode()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -14,20 +17,9 @@ class ViewController: UIViewController {
         sceneView.backgroundColor = UIColor(white: 0.7, alpha: 1.0)
 
         let scene = SCNScene()
-
         sceneView.scene = scene
 
-        let solidMaterial = SCNMaterial()
-        solidMaterial.diffuse.contents = UIColor.blue
-        solidMaterial.specular.contents = UIColor.darkGray
-        solidMaterial.shininess = 0.25
-
-        let tg = TerrainGenerator(width: 32, depth: 24)
-        let grid = tg.generate(level: 0)
-        let nodeFactory = NodeFactory(sideLength: 10.0)
-        let terrainNode = nodeFactory.createTerrainNode(grid: grid)
-        terrainNode.position = SCNVector3Make(0, 0, 0)
-        scene.rootNode.addChildNode(terrainNode)
+        createTerrain(in: scene)
 
         let light = SCNLight()
         light.type = .omni
@@ -37,20 +29,34 @@ class ViewController: UIViewController {
         lightNode.light = light
         lightNode.position = SCNVector3Make(0, 100, 0)
         scene.rootNode.addChildNode(lightNode)
+
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        sceneView.addGestureRecognizer(tapGesture)
     }
 
-    private func createGrid() -> Grid {
-        let grid = Grid(width: 24, depth: 12)
-        grid.build(at: GridPoint(x: 2, z: 2))
-        grid.build(at: GridPoint(x: 4, z: 2))
-        grid.build(at: GridPoint(x: 3, z: 1))
-        grid.build(at: GridPoint(x: 3, z: 3))
-        grid.build(at: GridPoint(x: 8, z: 8))
-        grid.build(at: GridPoint(x: 8, z: 8))
-        grid.build(at: GridPoint(x: 8, z: 8))
-        grid.build(at: GridPoint(x: 8, z: 8))
-        grid.build(at: GridPoint(x: 8, z: 8))
+    private func createTerrain(in scene: SCNScene) {
+        let tg = TerrainGenerator(width: 32, depth: 25)
+        let grid = tg.generate(level: terrainIndex)
+        let nodeFactory = NodeFactory(sideLength: 10.0)
 
-        return grid
+        terrainNode = nodeFactory.createTerrainNode(grid: grid)
+        terrainNode.position = SCNVector3Make(0, 0, 0)
+        scene.rootNode.addChildNode(terrainNode)
+
+        terrainIndex += 1
+    }
+
+    @objc
+    func handleTap(_ gestureRecognize: UIGestureRecognizer) {
+        guard
+            let sceneView = self.view as? SCNView,
+            let scene = sceneView.scene
+        else {
+            return
+        }
+
+        terrainNode.removeFromParentNode()
+
+        createTerrain(in: scene)
     }
 }
