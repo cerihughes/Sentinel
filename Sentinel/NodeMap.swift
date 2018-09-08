5100186b8143125f106790649780a4a6174e9970
Ceri Hughes <github@cerihughes.co.uk>
2018-09-08 19:41:58 +0100

    Can add and remove trees

--- Sentinel/NodeMap.swift
+++ Sentinel/NodeMap.swift
@@ -2,12 +2,18 @@ import SceneKit
 
 class NodeMap: NSObject {
     private var nodeMap: [SCNNode:GridPiece] = [:]
+    private var pointMap: [GridPoint:SCNNode] = [:]
 
     func add(node: SCNNode, for piece: GridPiece) {
         nodeMap[node] = piece
+        pointMap[piece.point] = node
     }
 
     func getPiece(for node: SCNNode) -> GridPiece? {
         return nodeMap[node]
+    }
+
+    func getNode(for point: GridPoint) -> SCNNode? {
+        return pointMap[point]
     }
 }
--- Sentinel/ViewController.swift
+++ Sentinel/ViewController.swift
@@ -29,8 +29,12 @@ class ViewController: UIViewController {
         sceneView.scene = viewModel.scene
         sceneView.delegate = viewModel
 
-        let tapRecogniser = UITapGestureRecognizer(target: self, action: #selector(tapGesture(_:)))
+        let tapRecogniser = UITapGestureRecognizer(target: self, action: #selector(tapGesture(sender:)))
         sceneView.addGestureRecognizer(tapRecogniser)
+
+        let longPressRecogniser = UILongPressGestureRecognizer(target: self, action: #selector(tapGesture(sender:)))
+        longPressRecogniser.isEnabled = false
+        sceneView.addGestureRecognizer(longPressRecogniser)
 
         let panRecogniser = UIPanGestureRecognizer(target: self, action: #selector(panGesture(sender:)))
         panRecogniser.isEnabled = false
@@ -38,21 +42,23 @@ class ViewController: UIViewController {
 
         viewModel.preAnimationBlock = {
             tapRecogniser.isEnabled = false
+            longPressRecogniser.isEnabled = false
             panRecogniser.isEnabled = false
         }
 
         viewModel.postAnimationBlock = {
             tapRecogniser.isEnabled = true
+            longPressRecogniser.isEnabled = true
             panRecogniser.isEnabled = true
         }
     }
 
     @objc
-    func tapGesture(_ gestureRecognizer: UIGestureRecognizer) {
-        if let sceneView = self.view as? SCNView {
-            let point = gestureRecognizer.location(in: sceneView)
+    func tapGesture(sender: UIGestureRecognizer) {
+        if let sceneView = self.view as? SCNView, let interaction = interaction(for: sender) {
+            let point = sender.location(in: sceneView)
             let hitTestResults = sceneView.hitTest(point, options: [:])
-            viewModel.process(hitTestResults: hitTestResults)
+            viewModel.process(interaction: interaction, hitTestResults: hitTestResults)
         }
     }
 
@@ -61,5 +67,17 @@ class ViewController: UIViewController {
         let translation = sender.translation(in: sender.view!)
         let deltaX = Float(translation.x)
         viewModel.processPan(by: deltaX, finished: sender.state == .ended)
+    }
+
+    private func interaction(for sender: UIGestureRecognizer) -> UserInteraction? {
+        if sender.isKind(of: UITapGestureRecognizer.self) {
+            return .tap
+        }
+
+        if sender.isKind(of: UILongPressGestureRecognizer.self) {
+            return .longPress
+        }
+
+        return nil
     }
 }
