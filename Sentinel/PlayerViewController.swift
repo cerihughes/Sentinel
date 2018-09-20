eb255aadd213828f15d683976b106e6117c4c33d
Ceri Hughes <github@cerihughes.co.uk>
2018-09-20 20:08:18 +0100

    More logic into container VC

--- Sentinel/PlayerViewController.swift
+++ Sentinel/PlayerViewController.swift
@@ -1,23 +1,7 @@
 import SceneKit
 import UIKit
 
-class PlayerViewController: UIViewController {
-    private let viewModel: ViewModel
-
-    init(viewModel: ViewModel) {
-        self.viewModel = viewModel
-
-        super.init(nibName: nil, bundle: nil)
-    }
-
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
-    override func loadView() {
-        view = SCNView()
-    }
-
+class PlayerViewController: OpponentViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -25,66 +9,6 @@ class PlayerViewController: UIViewController {
             return
         }
 
-        sceneView.scene = viewModel.playerScene
-        sceneView.backgroundColor = UIColor(white: 0.7, alpha: 1.0)
-        sceneView.pointOfView = viewModel.initialCameraNode
-
         sceneView.showsStatistics = true
-        sceneView.delegate = viewModel
-
-        let tapRecogniser = UITapGestureRecognizer(target: self, action: #selector(tapGesture(sender:)))
-        sceneView.addGestureRecognizer(tapRecogniser)
-
-        let longPressRecogniser = UILongPressGestureRecognizer(target: self, action: #selector(tapGesture(sender:)))
-        longPressRecogniser.isEnabled = false
-        sceneView.addGestureRecognizer(longPressRecogniser)
-
-        let panRecogniser = UIPanGestureRecognizer(target: self, action: #selector(panGesture(sender:)))
-        panRecogniser.isEnabled = false
-        sceneView.addGestureRecognizer(panRecogniser)
-
-        viewModel.preAnimationBlock = {
-            tapRecogniser.isEnabled = false
-            longPressRecogniser.isEnabled = false
-            panRecogniser.isEnabled = false
-        }
-
-        viewModel.postAnimationBlock = {
-            tapRecogniser.isEnabled = true
-            longPressRecogniser.isEnabled = true
-            panRecogniser.isEnabled = true
-        }
-    }
-
-    @objc
-    func tapGesture(sender: UIGestureRecognizer) {
-        if let sceneView = self.view as? SCNView, let interaction = interaction(for: sender) {
-            let point = sender.location(in: sceneView)
-            let hitTestResults = sceneView.hitTest(point, options: [:])
-            if viewModel.process(interaction: interaction, hitTestResults: hitTestResults) {
-                // Toggle the state to "complete" the gesture
-                sender.isEnabled = false
-                sender.isEnabled = true
-            }
-        }
-    }
-
-    @objc
-    func panGesture(sender: UIPanGestureRecognizer) {
-        let translation = sender.translation(in: sender.view!)
-        let deltaX = Float(translation.x)
-        viewModel.processPan(by: deltaX, finished: sender.state == .ended)
-    }
-
-    private func interaction(for sender: UIGestureRecognizer) -> UserInteraction? {
-        if sender.isKind(of: UITapGestureRecognizer.self) {
-            return .tap
-        }
-
-        if sender.isKind(of: UILongPressGestureRecognizer.self) {
-            return .longPress
-        }
-
-        return nil
     }
 }
