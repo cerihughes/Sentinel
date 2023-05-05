6bd68c3dced8051211bc61f133b4593f1b3d55df
cerihughes <github@cerihughes.co.uk>
2023-05-05 12:12:25 +0100

    Move grid creation API to Terrain Generator

--- Source/Grid/Grid.swift
+++ Source/Grid/Grid.swift
@@ -96,28 +96,6 @@ class Grid {
         }
     }
 
-    func build(at point: GridPoint) {
-        buildFloor(point: point)
-    }
-
-    func processSlopes() {
-        for z in 0 ..< depth {
-            var startPoint = GridPoint(x: 0, z: z)
-            processSlopes(from: startPoint, direction: .east)
-
-            startPoint = GridPoint(x: width - 1, z: z)
-            processSlopes(from: startPoint, direction: .west)
-        }
-
-        for x in 0 ..< width {
-            var startPoint = GridPoint(x: x, z: 0)
-            processSlopes(from: startPoint, direction: .south)
-
-            startPoint = GridPoint(x: x, z: depth - 1)
-            processSlopes(from: startPoint, direction: .north)
-        }
-    }
-
     func get(point: GridPoint) -> GridPiece? {
         let x = point.x, z = point.z
         guard
@@ -132,70 +110,5 @@ class Grid {
 
     var currentPiece: GridPiece? {
         return get(point: currentPosition)
-    }
-
-    private func buildFloor(point: GridPoint) {
-        guard let piece = get(point: point) else {
-            return
-        }
-
-        let slopeLevel = piece.buildFloor() - 0.5
-
-        for direction in GridDirection.allCases {
-            buildSlope(from: point, level: slopeLevel, direction: direction)
-        }
-    }
-
-    private func buildSlope(from point: GridPoint, level: Float, direction: GridDirection) {
-        let nextPoint = neighbour(of: point, direction: direction)
-        if let next = get(point: nextPoint) {
-            let nextLevel = next.level
-
-            if level <= nextLevel {
-                return
-            }
-
-            if level - nextLevel == 1.0 {
-                buildFloor(point: nextPoint)
-            }
-
-            let nextSlopeLevel = next.buildSlope() - 1.0
-
-            for direction in GridDirection.allValues(except: direction.opposite) {
-                buildSlope(from: nextPoint, level: nextSlopeLevel, direction: direction)
-            }
-        }
-    }
-
-    private func processSlopes(from point: GridPoint, direction: GridDirection) {
-        guard let firstPiece = get(point: point) else {
-            return
-        }
-
-        var slopeLevel = firstPiece.isFloor ? firstPiece.level - 0.5 : firstPiece.level - 1.0
-
-        var nextPoint = neighbour(of: point, direction: direction)
-        var next = get(point: nextPoint)
-        while next != nil {
-            let nextExists = next!
-            if nextExists.isFloor {
-                slopeLevel = nextExists.level - 0.5
-            } else {
-                if nextExists.level == slopeLevel {
-                    nextExists.add(slopeDirection: direction)
-                    slopeLevel -= 1.0
-                } else {
-                    slopeLevel = nextExists.level - 1.0
-                }
-            }
-
-            nextPoint = neighbour(of: nextPoint, direction: direction)
-            next = get(point: nextPoint)
-        }
-    }
-
-    private func neighbour(of point: GridPoint, direction: GridDirection) -> GridPoint {
-        let deltas = direction.toDeltas()
-        return point.transform(deltaX: deltas.x, deltaZ: deltas.z)
     }
 }
--- Source/Grid/GridPiece.swift
+++ Source/Grid/GridPiece.swift
@@ -11,30 +11,6 @@ class GridPiece {
         point = GridPoint(x: x, z: z)
     }
 
-    func buildFloor() -> Float {
-        if isFloor {
-            level += 1.0
-        } else {
-            isFloor = true
-            level += 0.5
-        }
-        return level
-    }
-
-    func buildSlope() -> Float {
-        if isFloor {
-            level += 0.5
-            isFloor = false
-        } else {
-            level += 1.0
-        }
-        return level
-    }
-
-    func add(slopeDirection: GridDirection) {
-        slopes |= slopeDirection.rawValue
-    }
-
     func has(slopeDirection: GridDirection) -> Bool {
         let rawValue = slopeDirection.rawValue
         return slopes & rawValue == rawValue
