6c16c71f99b15c7374a16392274a53f17c6bd35c
cerihughes <cerihughes@users.noreply.github.com>
2023-05-10 11:18:38 +0100

    Don't pass the energy into the main vc (#174)

--- Source/UI/Game/GameMainViewController.swift
+++ Source/UI/Game/GameMainViewController.swift
@@ -2,30 +2,18 @@ import SceneKit
 import UIKit
 
 class GameMainViewController: SceneViewController {
-    private let synthoidEnergy: SynthoidEnergy
-
-    init(scene: SCNScene, cameraNode: SCNNode?, overlay: OverlayScene, synthoidEnergy: SynthoidEnergy) {
-        self.synthoidEnergy = synthoidEnergy
-        super.init(scene: scene, cameraNode: cameraNode, overlay: overlay)
-    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        guard let sceneView = view as? SCNView else {
-            return
-        }
-
+        guard let sceneView = view as? SCNView else { return }
         sceneView.showsStatistics = true
     }
 
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
 
-        guard let overlay = overlay as? OverlayScene else {
-            return
-        }
-
-        overlay.updateEnergyUI(energy: synthoidEnergy.energy)
+        guard let overlay = overlay as? OverlayScene else { return }
+        overlay.refreshEnergyUI()
     }
 }
