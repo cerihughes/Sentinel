5a647af7edf24adb5483bdfc03e76536042526ec
cerihughes <github@cerihughes.co.uk>
2023-05-12 18:52:45 +0100

    Add more detection tests

--- Tests/UI/Game/Operations/OpponentsOperationsTests.swift
+++ Tests/UI/Game/Operations/OpponentsOperationsTests.swift
@@ -13,9 +13,104 @@ final class OpponentsOperationsTests: XCTestCase, TimeMachineTest {
         built.timeMachine
     }
 
-    override func setUpWithError() throws {
-        try super.setUpWithError()
-        let grid = Grid.createTestGrid()
+    override func tearDownWithError() throws {
+        view = nil
+        operations = nil
+        built = nil
+        worldBuilder = nil
+        try super.tearDownWithError()
+    }
+
+    func testDontDetectTree() {
+        let builder = GridBuilder.createGridBuilder()
+        var grid = builder.buildGrid()
+        grid.treePositions.append(.detectionPosition)
+
+        setupScene(grid: grid)
+
+        XCTAssertEqual(built.terrainOperations.grid.treePositions, [.detectionPosition])
+
+        runDetectionTest()
+
+        // There should be no change in the tree positions
+        XCTAssertEqual(built.terrainOperations.grid.treePositions, [.detectionPosition])
+    }
+
+    func testDetectTreeOnRock() {
+        let builder = GridBuilder.createGridBuilder()
+        var grid = builder.buildGrid()
+        grid.addRock(at: .detectionPosition)
+        grid.treePositions.append(.detectionPosition)
+
+        setupScene(grid: grid)
+
+        XCTAssertEqual(built.terrainOperations.grid.allRockPositions(), [.detectionPosition])
+        XCTAssertEqual(built.terrainOperations.grid.treePositions, [.detectionPosition])
+
+        runDetectionTest()
+
+        // The tree should have been detected and absorbed, creating another tree in the process
+        XCTAssertEqual(built.terrainOperations.grid.allRockPositions(), [.detectionPosition])
+        XCTAssertEqual(built.terrainOperations.grid.treePositions.count, 2)
+    }
+
+    func testDetectRock() {
+        let builder = GridBuilder.createGridBuilder()
+        var grid = builder.buildGrid()
+        grid.addRock(at: .detectionPosition)
+
+        setupScene(grid: grid)
+
+        XCTAssertEqual(built.terrainOperations.grid.allRockPositions(), [.detectionPosition])
+        XCTAssertEqual(built.terrainOperations.grid.treePositions, [])
+
+        runDetectionTest()
+
+        // A rock should have been detected and absorbed down to a tree, creating another tree in the process
+        XCTAssertEqual(built.terrainOperations.grid.allRockPositions(), [])
+        XCTAssertEqual(built.terrainOperations.grid.treePositions.count, 2)
+    }
+
+    func testDetectSynthoid() {
+        let builder = GridBuilder.createGridBuilder()
+        var grid = builder.buildGrid()
+        grid.synthoidPositions.append(.detectionPosition)
+
+        setupScene(grid: grid)
+
+        XCTAssertEqual(built.terrainOperations.grid.allRockPositions(), [])
+        XCTAssertEqual(built.terrainOperations.grid.synthoidPositions, [.startPosition, .detectionPosition])
+
+        runDetectionTest()
+
+        // A synthoid should have been detected and absorbed down to a rock, creating another tree in the process
+        XCTAssertEqual(built.terrainOperations.grid.allRockPositions(), [.detectionPosition])
+        XCTAssertEqual(built.terrainOperations.grid.synthoidPositions, [.startPosition])
+        XCTAssertEqual(built.terrainOperations.grid.treePositions.count, 1)
+    }
+
+    func testDetectSynthoidOnRock() {
+        let builder = GridBuilder.createGridBuilder()
+        var grid = builder.buildGrid()
+        grid.addRock(at: .detectionPosition)
+        grid.synthoidPositions.append(.detectionPosition)
+
+        setupScene(grid: grid)
+
+        XCTAssertEqual(built.terrainOperations.grid.allRockPositions(), [.detectionPosition])
+        XCTAssertEqual(built.terrainOperations.grid.rockCount(at: .detectionPosition), 1)
+        XCTAssertEqual(built.terrainOperations.grid.synthoidPositions, [.startPosition, .detectionPosition])
+
+        runDetectionTest()
+
+        // A synthoid should have been detected and absorbed down to a rock, creating another tree in the process
+        XCTAssertEqual(built.terrainOperations.grid.allRockPositions(), [.detectionPosition])
+        XCTAssertEqual(built.terrainOperations.grid.rockCount(at: .detectionPosition), 2)
+        XCTAssertEqual(built.terrainOperations.grid.synthoidPositions, [.startPosition])
+        XCTAssertEqual(built.terrainOperations.grid.treePositions.count, 1)
+    }
+
+    private func setupScene(grid: Grid) {
         worldBuilder = WorldBuilder.createMock(grid: grid)
         built = worldBuilder.build()
         operations = built.opponentsOperations
@@ -26,23 +121,12 @@ final class OpponentsOperationsTests: XCTestCase, TimeMachineTest {
         view.pointOfView = built.initialCameraNode
     }
 
-    override func tearDownWithError() throws {
-        view = nil
-        operations = nil
-        built = nil
-        worldBuilder = nil
-        try super.tearDownWithError()
-    }
-
-    func testGenerateTree() {
+    private func runDetectionTest() {
         var timeMachineCompletedAllOperations = false
         _ = timeMachine.add(timeInterval: 2.0) { _, _, _ in
             timeMachineCompletedAllOperations = true
             return true
         }
-
-        XCTAssertEqual(built.terrainOperations.grid.allRockPositions().count, 1)
-        XCTAssertEqual(built.terrainOperations.grid.treePositions.count, 0)
 
         timeMachine.start()
         XCTAssertTrue(built.playerOperations.enterScene())
@@ -50,22 +134,22 @@ final class OpponentsOperationsTests: XCTestCase, TimeMachineTest {
         while !timeMachineCompletedAllOperations {
             pumpRunLoop()
         }
-
-        // A rock should have been detected and absorbed down to a tree, creating another tree in the process
-        XCTAssertEqual(built.terrainOperations.grid.allRockPositions().count, 0)
-        XCTAssertEqual(built.terrainOperations.grid.treePositions.count, 2)
     }
+
 }
 
-private extension Grid {
-    static func createTestGrid() -> Grid {
+private extension GridPoint {
+    static let startPosition = GridPoint(x: 0, z: 2)
+    static let sentinelPosition = GridPoint(x: 2, z: 2)
+    static let detectionPosition = GridPoint(x: 4, z: 2)
+}
+
+private extension GridBuilder {
+    static func createGridBuilder() -> GridBuilder {
         let builder = GridBuilder(width: 5, depth: 5)
-        let startPosition = GridPoint(x: 0, z: 2)
-        builder.startPosition = startPosition
-        builder.synthoidPositions.append(startPosition)
-        builder.sentinelPosition = .init(x: 2, z: 2)
-        var grid = builder.buildGrid()
-        grid.addRock(at: .init(x: 4, z: 2))
-        return grid
+        builder.startPosition = .startPosition
+        builder.synthoidPositions.append(.startPosition)
+        builder.sentinelPosition = .sentinelPosition
+        return builder
     }
 }
