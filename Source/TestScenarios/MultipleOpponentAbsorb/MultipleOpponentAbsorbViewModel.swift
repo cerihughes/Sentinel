1e813fdfe8c97481d9f26ea7312960cd4ce78ebd
cerihughes <cerihughes@users.noreply.github.com>
2023-06-01 13:45:20 +0100

    Use world builder in test scenario (#197)

--- Source/TestScenarios/MultipleOpponentAbsorb/MultipleOpponentAbsorbViewModel.swift
+++ Source/TestScenarios/MultipleOpponentAbsorb/MultipleOpponentAbsorbViewModel.swift
@@ -3,55 +3,38 @@ import SceneKit
 
 /// A test scenario to make sure multiple opponents don't try and absorb the same objects at the same time.
 class MultipleOpponentAbsorbViewModel {
-    let scene = SCNScene()
-    let initialCameraNode: SCNNode
-    let timeMachine: TimeMachine
-    private let opponentsOperations: OpponentsOperations
-    private let terrainOperations: TerrainOperations
+    private let terrain: WorldBuilder.Terrain
+    private let operations: WorldBuilder.Operations
     private let initialTrees: Int
     private let rocksCreated: Int
     private var absorbed = 0
 
+    var scene: SCNScene { terrain.scene }
+    var cameraNode: SCNNode { terrain.initialCameraNode }
+    var timeMachine: TimeMachine { operations.timeMachine }
+
     init() {
-        let level = 45
-        let levelConfiguration = DefaultLevelConfiguration(level: level)
-        let terrainGenerator = DefaultTerrainGenerator(levelConfiguration: levelConfiguration)
-        let materialFactory = DefaultMaterialFactory(level: level)
-        var grid = terrainGenerator.generate()
-        initialTrees = grid.treePositions.count
-        rocksCreated = grid.addRockNodesToLowestLevel()
+        let builder = WorldBuilder.createDefault(level: 45)
+        terrain = builder.buildTerrain(initialCameraPosition: .init(50, 300, 300))
+        operations = terrain.createOperations()
+        initialTrees = terrain.terrainOperations.grid.treePositions.count
+        rocksCreated = terrain.terrainOperations.addRockNodesToLowestLevel()
 
-        let nodeMap = NodeMap()
-        let nodePositioning = grid.createNodePositioning()
-        let nodeFactory = NodeFactory(nodePositioning: nodePositioning, materialFactory: materialFactory)
-        let terrainNode = nodeFactory.createTerrainNode(grid: grid, nodeMap: nodeMap)
-        let world = SpaceWorld()
-        world.buildWorld(in: scene, around: terrainNode)
-
-        initialCameraNode = nodeFactory.createSynthoidNode(height: 0, viewingAngle: 0.0).cameraNode
-        initialCameraNode.position = SCNVector3Make(50.0, 300, 300)
-        initialCameraNode.look(at: terrainNode.worldPosition)
-
-        terrainNode.addChildNode(initialCameraNode)
-
-        let nodeManipulator = NodeManipulator(terrainNode: terrainNode, nodeFactory: nodeFactory, animatable: true)
-        nodeManipulator.currentSynthoidNode = nodeMap.synthoidNode(at: grid.startPosition)
-
-        terrainOperations = TerrainOperations(grid: grid, nodeMap: nodeMap, nodeManipulator: nodeManipulator)
-        timeMachine = .init()
-        opponentsOperations = .init(terrainOperations: terrainOperations, timeMachine: timeMachine)
-        opponentsOperations.delegate = self
-        timeMachine.start()
+        operations.opponentsOperations.delegate = self
+        operations.playerOperations.enterScene()
+        operations.timeMachine.start()
     }
 }
 
 extension MultipleOpponentAbsorbViewModel: OpponentsOperationsDelegate {
+    private var grid: Grid { terrain.terrainOperations.grid }
+
     func opponentsOperationsDidAbsorb(_: OpponentsOperations) {
         absorbed += 1
 
         if absorbed == expectedAbsorptions {
-            assert(terrainOperations.grid.allRockPositions().isEmpty)
-            assert(terrainOperations.grid.treePositions.count == expectedTrees)
+            assert(grid.allRockPositions().isEmpty)
+            assert(grid.treePositions.count == expectedTrees)
         }
     }
 
@@ -71,12 +54,12 @@ extension MultipleOpponentAbsorbViewModel: OpponentsOperationsDelegate {
     func opponentsOperations(_: OpponentsOperations, didEndDetectOpponent cameraNode: SCNNode) {}
 }
 
-private extension Grid {
-    mutating func addRockNodesToLowestLevel() -> Int {
-        let points = emptyFloorPieces()
+private extension TerrainOperations {
+    func addRockNodesToLowestLevel() -> Int {
+        let points = grid.emptyFloorPieces()
             .filter { Int($0.level) > 1 }
             .map { $0.point }
-        points.forEach { addRock(at: $0) }
+        points.forEach { buildRock(at: $0) }
         return points.count
     }
 }
